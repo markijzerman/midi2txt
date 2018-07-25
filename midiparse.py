3603e8eeac8291e68a869e218ce41427d519b2ba
Mark IJzerman <markijzerman@gmail.com>
2018-07-25 17:13:36 +0200

    different structure and takes chords as well

--- midiparse.py
+++ midiparse.py
@@ -5,15 +5,31 @@ from music21 import converter
 pathname = os.path.abspath(os.path.dirname(sys.argv[0]))
 
 # parse notes of midi file that is passed through argument in command line
-parseNotes = converter.parse(pathname + '/' + sys.argv[1])
+curMidiFile = converter.parse(pathname + '/' + sys.argv[1])
 
 # open file to write to
 f = open(sys.argv[1][:-3] + 'txt', 'w')
 
 
-for p in parseNotes.parts:
-    for n in p.flat.notes:
-        print ("%s %s" % (n.pitch.midi, n.duration.quarterLength))
-        f.write("%s %s \n" % (n.pitch.midi, n.duration.quarterLength))
+for part in curMidiFile.parts:
+    for event in part:
 
+        if getattr(event, 'isChord', None) and event.isChord:
+            tempchordlist = []
+            tempchordlist = list(event.pitches)
+            finishchordlist = []
+            for item in tempchordlist:
+                finishchordlist.append(item.midi)
+            print ("%s %s %s" % (event.offset, event.duration.quarterLength, " ".join([str(x) for x in finishchordlist])))
+            f.write("%s %s %s \n" % (event.offset, event.duration.quarterLength, " ".join([str(x) for x in finishchordlist])))
+
+        if getattr(event, 'isNote', None) and event.isNote:
+            print ("%s %s %s %s" % ("NOTE", event.pitch.midi, event.duration.quarterLength, event.offset))
+            f.write("%s %s %s %s \n" % ("NOTE", event.pitch.midi, event.duration.quarterLength, event.offset))
+
+        if getattr(event, 'isRest', None) and event.isRest:
+            print("%s %s" % ("REST", event.duration.quarterLength))
+            f.write("%s %s \n" % ("REST", event.duration.quarterLength))
+    
 f.close()
+
